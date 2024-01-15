8d316a79dbbce0214f3c728f476dfa88877c1106
s.vyazovik <s.vyazovik@cloudcom.ru>
2024-01-15 11:31:16 +0300

    3. Вызов конструктора родителя super(...) [#6837]

---
+++ src/main/java/ru/job4j/inheritance/Engineer.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+public class Engineer extends Profession {
+    private int experience;
+
+    public Engineer() {
+        super();
+        this.experience = experience;
+    }
+}
