05d31b065f05ac1f1811f8573df1e32a8953b688
s.vyazovik <s.vyazovik@cloudcom.ru>
2024-03-11 13:46:28 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -32,4 +36,7 @@ public class Item {
         this.name = name;
     }
 
+    public LocalDateTime getCreated() {
+        return created;
+    }
 }
