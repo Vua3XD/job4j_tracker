df5b54f42569e3751c692aaf7d48648c54330ac0
s.vyazovik <s.vyazovik@cloudcom.ru>
2024-03-13 13:12:52 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -72,4 +72,23 @@ public class TrackerTest {
         Item updateItem = new Item("Bug with description");
         boolean result = tracker.replace(1000, updateItem);
     }
+
+    @Test
+    public void whenDeleteItemIsSuccessful() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Bug");
+        tracker.add(item);
+        int id = item.getId();
+        tracker.delete(id);
+        assertThat(tracker.findById(id)).isNull();
+    }
+
+    @Test
+    public void whenDeleteItemIsNotSuccessful() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Bug");
+        tracker.add(item);
+        tracker.delete(1000);
+        assertThat(tracker.findById(item.getId()).getName()).isEqualTo("Bug");
+    }
 }
