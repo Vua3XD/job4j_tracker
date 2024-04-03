3897470c86da8b3d10aba5473157af9d687ddc9c
s.vyazovik <s.vyazovik@cloudcom.ru>
2024-04-03 11:19:30 +0300

    9.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,6 +3,8 @@ package ru.job4j.tracker;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 class StartUITest {
     @Test
@@ -14,5 +16,28 @@ class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName()).isEqualTo(expected.getName());
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        String[] input = {"1", "test name"};
+        MockInput mockInput = new MockInput(input);
+        Tracker testTracker = new Tracker();
+
+        StartUI.replaceItem(mockInput, testTracker);
+
+        assertEquals("test name", testTracker.findById(1).getName());
+    }
+
+    @Test
+    public void testDeleteItem() {
+        String[] input = {"1"};
+        MockInput mockInput = new MockInput(input);
+        Tracker testTracker = new Tracker();
+        testTracker.add(new Item("Test Item"));
+
+        StartUI.deleteItem(mockInput, testTracker);
+
+        assertNull(testTracker.findById(1));
     }
 }
