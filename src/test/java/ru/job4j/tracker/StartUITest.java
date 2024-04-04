5f532f1a298712f823277b6f6f4d9687a768724b
s.vyazovik <s.vyazovik@cloudcom.ru>
2024-04-04 10:03:52 +0300

    12.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -48,4 +48,45 @@ class StartUITest {
         new StartUI(new ConsoleOutput()).init(input, tracker, actions);
         assertThat(tracker.findById(item.getId())).isNull();
     }
+
+    @Test
+    void whenFindAllItems() {
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("Item_1"));
+        Item item2 = tracker.add(new Item("Item_2"));
+        Input input = new MockInput(new String[] {"0", "1"});
+        UserAction[] actions = {
+                new FindAllAction(new ConsoleOutput()),
+                new ExitAction(new ConsoleOutput())
+        };
+        new StartUI(new ConsoleOutput()).init(input, tracker, actions);
+        assertThat(tracker.findAll()).containsExactly(item1, item2);
+    }
+
+    @Test
+    void whenFindItemsByName() {
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("Item_1"));
+        Item item2 = tracker.add(new Item("Item_2"));
+        Input input = new MockInput(new String[] {"0", "Item_1", "1"});
+        UserAction[] actions = {
+                new FindByNameAction(new ConsoleOutput()),
+                new ExitAction(new ConsoleOutput())
+        };
+        new StartUI(new ConsoleOutput()).init(input, tracker, actions);
+        assertThat(tracker.findByName("Item_1")).containsExactly(item1);
+    }
+
+    @Test
+    void whenFindItemById() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Item"));
+        Input input = new MockInput(new String[] {"0", String.valueOf(item.getId()), "1"});
+        UserAction[] actions = {
+                new FindByIdAction(new ConsoleOutput()),
+                new ExitAction(new ConsoleOutput())
+        };
+        new StartUI(new ConsoleOutput()).init(input, tracker, actions);
+        assertThat(tracker.findById(item.getId())).isEqualTo(item);
+    }
 }
