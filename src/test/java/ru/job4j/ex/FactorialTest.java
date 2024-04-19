74500e099d04b3df094d00e558ab04857c0c45c1
s.vyazovik <s.vyazovik@cloudcom.ru>
2024-04-19 10:30:16 +0300

    0.3. Тестирование исключений с junit [#219366 #491367]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -1,14 +1,35 @@
 package ru.job4j.ex;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import ru.job4j.ex.Factorial;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FactorialTest {
 
     @Test
-    public void whenNumberIsNegativeThenThrowIllegalArgumentException() {
+    public void whenNumberIsNegativeThenThrowException() {
         Factorial factorial = new Factorial();
-        assertThrows(IllegalArgumentException.class, () -> factorial.calc(-1));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            factorial.calc(-1);
+        });
+    }
+
+    @Test
+    public void whenNumberIsZeroThenReturnOne() {
+        Factorial factorial = new Factorial();
+        int result = factorial.calc(0);
+
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void whenNumberIsPositiveThenCalculateFactorial() {
+        Factorial factorial = new Factorial();
+        int result = factorial.calc(5);
+
+        assertEquals(120, result);
     }
 }
