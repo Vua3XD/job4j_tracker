30dea0bb94a66c4ecf231f637944e88efeafb093
s.vyazovik <s.vyazovik@cloudcom.ru>
2024-04-25 14:00:02 +0300

    2.1. Тест на ValidateInput [#311887]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -1,47 +1,37 @@
 package ru.job4j.tracker;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import ru.job4j.tracker.Input;
+import ru.job4j.tracker.MockInput;
+import ru.job4j.tracker.StubOutput;
+import ru.job4j.tracker.ValidateInput;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-class ValidateInputTest {
+public class ValidateInputTest {
 
     @Test
-    void whenValidInput() {
-        Output output = new StubOutput();
-        Input in = new MockInput(
-                new String[] {"5"}
-        );
-        ValidateInput input = new ValidateInput(output, in);
-        int selected = input.askInt("Enter menu:");
-        assertThat(selected).isEqualTo(5);
+    public void testValidInput() {
+        Input input = new MockInput(new String[]{"5"});
+        ValidateInput validateInput = new ValidateInput(new StubOutput(), input);
+        int result = validateInput.askInt("Введите число:");
+        assertThat(result).isEqualTo(5);
     }
 
     @Test
-    void whenMultipleValidInputs() {
-        Output output = new StubOutput();
-        Input in = new MockInput(
-                new String[] {"1", "2", "3"}
-        );
-        ValidateInput input = new ValidateInput(output, in);
-
-        int selected1 = input.askInt("Enter menu:");
-        assertThat(selected1).isEqualTo(1);
-
-        int selected2 = input.askInt("Enter menu:");
-        assertThat(selected2).isEqualTo(2);
-
-        int selected3 = input.askInt("Enter menu:");
-        assertThat(selected3).isEqualTo(3);
+    public void testMultipleValidInputs() {
+        Input input = new MockInput(new String[]{"1", "2", "3"});
+        ValidateInput validateInput = new ValidateInput(new StubOutput(), input);
+        assertThat(validateInput.askInt("Введите число:")).isEqualTo(1);
+        assertThat(validateInput.askInt("Введите число:")).isEqualTo(2);
+        assertThat(validateInput.askInt("Введите число:")).isEqualTo(3);
     }
 
     @Test
-    void whenNegativeInput() {
-        Output output = new StubOutput();
-        Input in = new MockInput(
-                new String[] {"-10"}
-        );
-        ValidateInput input = new ValidateInput(output, in);
-        int selected = input.askInt("Enter menu:");
-        assertThat(selected).isEqualTo(-10);
+    public void testNegativeInput() {
+        Input input = new MockInput(new String[]{"-10"});
+        ValidateInput validateInput = new ValidateInput(new StubOutput(), input);
+        assertThat(validateInput.askInt("Введите число:")).isEqualTo(-10);
     }
+
 }
