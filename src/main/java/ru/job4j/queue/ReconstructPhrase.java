9830ed0aa5abc3fa1661f017c325ef1eac2d3072
s.vyazovik <s.vyazovik@cloudcom.ru>
2024-05-03 16:01:05 +0300

    2. Интерфейс Deque [#504943]

---
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -0,0 +1,43 @@
+package ru.job4j.queue;
+
+import java.util.Deque;
+
+public class ReconstructPhrase {
+
+    private final Deque<Character> descendingElements;
+
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder result = new StringBuilder();
+        int index = 0;
+        while (!evenElements.isEmpty()) {
+            char currentChar = evenElements.pollFirst();
+            if (index % 2 == 0) {
+                result.append(currentChar);
+            }
+            index++;
+        }
+        return result.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder result = new StringBuilder();
+        while (!descendingElements.isEmpty()) {
+            result.append(descendingElements.pollLast());
+        }
+        return result.toString();
+    }
+
+    public String getReconstructPhrase() {
+        StringBuilder result = new StringBuilder();
+        result.append(getEvenElements());
+        result.append(getDescendingElements());
+        return result.toString();
+    }
+}
